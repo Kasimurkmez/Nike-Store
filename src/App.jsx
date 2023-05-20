65206ff95f34146cf677ec9afb906571c86400e4
kasimurkmez <kasimurkmez47@gmail.com>
2023-05-21 00:15:21 +0300

    update

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import {
+  Cart,
+  FlexContent,
+  Footer,
+  Hero,
+  Navbar,
+  Sales,
+  Stories,
+} from "./components";
+import {
+  heroapi,
+  popularsales,
+  toprateslaes,
+  highlight,
+  sneaker,
+  story,
+  footerAPI,
+} from "./data/data.js";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Cart />
+      <main className="flex flex-col gap-16 relative">
+        <Hero heroapi={heroapi} />
+        <Sales endpoint={popularsales} ifExists />
+        <FlexContent endpoint={highlight} ifExists />
+        <Sales endpoint={toprateslaes} />
+        <FlexContent endpoint={sneaker} />
+        <Stories story={story} />
+      </main>
+      <Footer footerAPI={footerAPI} />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero.jsx";
+import Sales from "./Sales.jsx";
+import FlexContent from "./FlexContent.jsx";
+import Stories from "./Stories.jsx";
+import Footer from "./Footer.jsx";
+import Navbar from "./Navbar.jsx";
+import Cart from "./Cart.jsx";
+
+export { Hero, Sales, FlexContent, Stories, Footer, Navbar, Cart };
+
+//  Bu şekilde olmuyor neden acaba ????
+
+/* export { default as Navbar } from './Navbar.jsx';
+export { default as Cart } from './Cart.jsx';
+export { default as Hero } from './Hero.jsx';
+export { default as Sales } from './Sales.jsx';
+export { default as FlexContent } from './FlexContent.jsx';
+export { default as Stories } from './Stories.jsx';
+export { default as Footer } from './Footer.jsx'; */
